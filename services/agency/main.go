402cc107f8cbacc8eb147c76a724badde5c2b6b9
Stev Witzel <switzel@pivotal.io>
2015-08-27 00:48:30 +0100

    Add circuit breaker to agency service

--- services/agency/main.go
+++ services/agency/main.go
@@ -2,37 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/afex/hystrix-go/hystrix"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	hystrixStreamHandler := hystrix.NewStreamHandler()
+	hystrixStreamHandler.Start()
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
+	router.HandleFunc("/stream", hystrixStreamHandler.ServeHTTP)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router))
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	name := guide()
-	fmt.Fprintf(w, "Your guide will be: %s", name)
+	hystrix.Do("assign_guide", assignGuide(w), fallback(w))
 }
 
-func guide() string {
-	resp, err := http.Get("http://company.cfapps.pez.pivotal.io/available")
-	if err != nil {
-		return ""
-	}
+func assignGuide(w io.Writer) func() error {
+	return func() error {
+		resp, err := http.Get("http://company.cfapps.pez.pivotal.io/available")
+		if err != nil {
+			return err
+		}
 
-	guide, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return ""
-	}
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("Error requesting guide. HTTP %d", resp.StatusCode)
+		}
 
-	return string(guide)
+		guide, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(w, "Your guide will be: %s", guide)
+		return nil
+	}
+}
+
+func fallback(w io.Writer) func(error) error {
+	return func(error) error {
+		fmt.Fprint(w, "We will get back to you shortly.")
+		return nil
+	}
 }
